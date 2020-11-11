86ce5395ecadfe9617cef3df7e9e4fcb3b7fbc6b
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-11-10 21:54:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,6 +51,46 @@ client.on('message', message => {
             message.reply(embed);
         }
     }
+
+
+    else if (command === "ban"){
+        const user = message.mentions.users.first();
+        if (user){
+            const member = message.guild.member(user);
+            if (member){
+                member
+                    .kick()
+                    .then(() => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor("#FF0000")
+                            .setTitle("Banned Member")
+                            .setDescription(`Banned ${user.tag}`);
+                        message.reply(embed);
+                    })
+                    .catch(error => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor("#FF0000")
+                            .setTitle("Error")
+                            .setDescription(`${error}`);
+                        message.reply(embed);
+                    });
+            }
+            else{
+                const embed = new Discord.MessageEmbed()
+                    .setColor("#FF0000")
+                    .setTitle("Error")
+                    .setDescription("User not in Guild");
+                message.reply(embed);
+            }
+        }
+        else{
+            const embed = new Discord.MessageEmbed()
+                .setColor("#FF0000")
+                .setTitle("Error")
+                .setDescription("Incorrect Command Ussage");
+            message.reply(embed);
+        }
+    }
 });
 
 client.login(token);
