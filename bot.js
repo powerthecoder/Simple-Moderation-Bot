8c9af16e748bc16e43dfc297a5a5192bda9e4438
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-11-10 21:54:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,48 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+    if (!message.guild) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
+
+    if (command === "kick"){
+        const user = message.mentions.users.first();
+        if (user){
+            const member = message.guild.member(user);
+            if (member){
+                member
+                    .kick()
+                    .then(() => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor("#FF0000")
+                            .setTitle("Kicked Member")
+                            .setDescription(`Kicked ${user.tag}`);
+                        message.reply(embed);
+                    })
+                    .catch(error => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor("#FF0000")
+                            .setTitle("Error")
+                            .setDescription(`${error}`);
+                        message.reply(embed);
+                    });
+            }
+            else{
+                const embed = new Discord.MessageEmbed()
+                    .setColor("#FF0000")
+                    .setTitle("Error")
+                    .setDescription("User not in Guild");
+                message.reply(embed);
+            }
+        }
+        else{
+            const embed = new Discord.MessageEmbed()
+                .setColor("#FF0000")
+                .setTitle("Error")
+                .setDescription("Incorrect Command Ussage");
+            message.reply(embed);
+        }
+    }
 });
 
 client.login(token);
